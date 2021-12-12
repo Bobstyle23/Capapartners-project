dcfc454dde9bd82afaefcbad509230247aeaf6b1
Bob Pakhriev <boburpakhriev@gmail.com>
2021-12-12 12:34:09 +0900

    Fetch mock data trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css";
-
+import datas from "../src/data.json";
 function App() {
   return (
     <div className="App">
       <Header />
+      <p>
+        {datas.requests.map((data) => (
+          <>
+            <h1>{data.client}</h1>
+            <p>{data.amount}</p>
+          </>
+        ))}
+      </p>
     </div>
   );
 }
