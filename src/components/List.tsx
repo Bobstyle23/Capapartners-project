fe3015aadac09084032eafe5b474fd4f63afd821
Bob Pakhriev <boburpakhriev@gmail.com>
2021-12-12 15:25:41 +0900

    Add dropdown & badge

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import datas from "../data.json";
-import { Card, Container, Row, Button } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Row,
+  Button,
+  Badge,
+  DropdownButton,
+  Dropdown,
+  Form,
+} from "react-bootstrap";
+
 const List = () => {
   const items = datas.requests;
   const method = items.map((item) => item.method);
@@ -13,6 +23,37 @@ const List = () => {
     <div>
       <Container>
         <Row style={{ margin: "5rem" }}>
+          <h3
+            style={{
+              color: "#323D45",
+              textAlign: "left",
+            }}
+          >
+            들어온 요청
+          </h3>
+          <p
+            style={{
+              color: "#323D45",
+              textAlign: "left",
+            }}
+          >
+            파트너님에게 딱 맞는 요청서를 찾아보세요.
+          </p>
+          <DropdownButton
+            id="dropdown-basic-button"
+            variant="light"
+            title="가공방식"
+            style={{
+              backgroundColor: "#ffffff",
+              display: "flex",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
+          >
+            <Form.Check id="1" label="밀링" />
+            <Form.Check id="2" label="선반" />
+          </DropdownButton>
+
           {items.map((item) => (
             <Card
               key={item.id}
@@ -24,7 +65,27 @@ const List = () => {
             >
               <Card.Body>
                 <Card.Title>{item.title}</Card.Title>
+                {item.status === "상담중" ? (
+                  <span>
+                    <Badge
+                      pill
+                      bg="light"
+                      style={{
+                        backgroundColor: "#ffffff",
+                        border: "1px solid #FFA000",
+                        color: "#FFA000",
+                        float: "right",
+                        marginTop: "-2rem",
+                      }}
+                    >
+                      상담중
+                    </Badge>
+                  </span>
+                ) : (
+                  ""
+                )}
                 <h5>{item.client}</h5>
+
                 <Card.Subtitle className="mb-2 text-muted">
                   {item.due}
                 </Card.Subtitle>
