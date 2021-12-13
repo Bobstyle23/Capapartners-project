a8d59059ec62f5dfbfac2dea823035ecff7940fa
Bob Pakhriev <boburpakhriev@gmail.com>
2021-12-14 07:13:02 +0900

    Add icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
+import DomainIcon from "@material-ui/icons/Domain";
 import "./styles/header-styles.css";
 const Header = () => {
   return (
@@ -26,6 +27,9 @@ const Header = () => {
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link className="link-color" href="#action1">
+                  <DomainIcon
+                    style={{ marginBottom: "0.65rem", marginRight: "0.3rem" }}
+                  />
                   파트너정밀가공
                 </Nav.Link>
                 <Nav.Link className="link-color" href="#action2">
@@ -36,6 +40,7 @@ const Header = () => {
           </Navbar.Offcanvas>
           <Navbar.Collapse>
             <Nav className="justify-content-end flex-grow-1">
+              <DomainIcon style={{ color: "#ffffff", marginTop: "0.375rem" }} />
               <Nav.Link id="header-link">A 가공 업체</Nav.Link>
               <Nav.Link id="header-link2">로그아웃</Nav.Link>
             </Nav>
