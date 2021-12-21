373a1145ab5e216ca2d21e0f3733532d7d2548f4
Bob Pakhriev <boburpakhriev@gmail.com>
2021-12-21 11:29:50 +0900

    Add new components to split

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import "./App.css";
+import datas from "../src/data.json";
 
 function App() {
+  const items = datas.requests;
+
   return (
     <div className="App">
       <Header />
